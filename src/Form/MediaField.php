d7837512e595a2dac61784231d21fee29ba341d7
Erik Frèrejean <erik@wedevelop.nl>
2026-01-22 10:58:17 +0100

    Add configurable media types (enable/disable)
    Allow developers to enable/disable built-in media types (Image, Video)
    via SilverStripe's YAML configuration system using the `enabled_types`
    config property.
    
    BREAKING CHANGE: Wrapper/field getter return types now nullable:
    - getVideoWrapper(): ?Wrapper
    - getImageWrapper(): ?Wrapper
    - getImageUploadField(): ?UploadField

--- src/Form/MediaField.php
+++ src/Form/MediaField.php
@@ -4,27 +4,41 @@ namespace WeDevelop\MediaField\Form;
 
 use Embed\Embed;
 use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Forms\CompositeField;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
-use UncleCheese\DisplayLogic\Forms\Wrapper;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\FieldType\DBHTMLText;
+use UncleCheese\DisplayLogic\Forms\Wrapper;
 
 class MediaField extends CompositeField
 {
-    private Wrapper $videoWrapper;
+    use Configurable;
 
-    private Wrapper $imageWrapper;
+    /**
+     * Which media types are enabled
+     * @config
+     * @var array<string, bool>
+     */
+    private static array $enabled_types = [
+        'image' => true,
+        'video' => true,
+    ];
 
-    private UploadField $imageUploadField;
+    private ?Wrapper $videoWrapper = null;
+
+    private ?Wrapper $imageWrapper = null;
+
+    private ?UploadField $imageUploadField = null;
 
     private string $typeField;
 
     public function __construct(FieldList $fields, string $mediaUploadFolder = 'MediaUploads', string $typeField = 'MediaType', string $imageField = 'MediaImage', string $videoField = 'MediaVideoFullURL')
     {
         $this->typeField = $typeField;
+        $enabledTypes = $this->config()->get('enabled_types');
 
         $fields->removeByName([
             $typeField,
@@ -34,20 +48,30 @@ class MediaField extends CompositeField
 
         $children = [];
 
-        // Type
-        $children[] = DropdownField::create($typeField, 'Type', MediaType::toDropdownSource());
+        // Type dropdown (filtered by enabled types)
+        $dropdownSource = [];
+        foreach (MediaType::cases() as $type) {
+            if ($enabledTypes[$type->value] ?? true) {
+                $dropdownSource[$type->value] = $type->label();
+            }
+        }
+        $children[] = DropdownField::create($typeField, 'Type', $dropdownSource);
 
-        // Image
-        $this->imageWrapper = Wrapper::create(
-            $this->imageUploadField = UploadField::create($imageField, 'Image')->setFolderName($mediaUploadFolder)
-        );
+        // Image (only if enabled)
+        if ($enabledTypes[MediaType::Image->value] ?? true) {
+            $this->imageWrapper = Wrapper::create(
+                $this->imageUploadField = UploadField::create($imageField, 'Image')->setFolderName($mediaUploadFolder)
+            );
+            $children[] = $this->imageWrapper;
+        }
 
-        // Video
-        $this->videoWrapper = Wrapper::create(
-            TextField::create($videoField)
-        );
-
-        array_push($children, $this->imageWrapper, $this->videoWrapper);
+        // Video (only if enabled)
+        if ($enabledTypes[MediaType::Video->value] ?? true) {
+            $this->videoWrapper = Wrapper::create(
+                TextField::create($videoField)
+            );
+            $children[] = $this->videoWrapper;
+        }
 
         parent::__construct($children);
     }
@@ -55,23 +79,23 @@ class MediaField extends CompositeField
     /** @param array<string, mixed> $properties */
     public function FieldHolder($properties = []): DBHTMLText
     {
-        $this->imageWrapper->displayIf($this->typeField)->isEqualTo(MediaType::Image->value);
-        $this->videoWrapper->displayIf($this->typeField)->isEqualTo(MediaType::Video->value);
+        $this->imageWrapper?->displayIf($this->typeField)->isEqualTo(MediaType::Image->value);
+        $this->videoWrapper?->displayIf($this->typeField)->isEqualTo(MediaType::Video->value);
 
         return parent::FieldHolder($properties);
     }
 
-    public function getVideoWrapper(): Wrapper
+    public function getVideoWrapper(): ?Wrapper
     {
         return $this->videoWrapper;
     }
 
-    public function getImageWrapper(): Wrapper
+    public function getImageWrapper(): ?Wrapper
     {
         return $this->imageWrapper;
     }
 
-    public function getImageUploadField(): UploadField
+    public function getImageUploadField(): ?UploadField
     {
         return $this->imageUploadField;
     }
